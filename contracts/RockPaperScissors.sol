71ecd0d3bdc2922b14febf20fcd7fd12b92f6dc3
Cristopher Ortega <ortega.cpp@gmail.com>
2021-05-22 23:15:27 -0400

    chore: improve certain variable names

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -21,14 +21,14 @@ contract RockPaperScissors {
 
     uint internal constant REVEALED_MOVE_MISSING = uint8(0);
     uint8 internal constant ROUND_INITIALIZED = uint8(1);
-    uint8 internal constant REVEALED_MOVE_REFUSED = uint8(2);
+    uint8 internal constant REVEALED_MOVE_REFUSED = uint8(4);
     // Rock, Paper and Scissors (3)
-    uint8 internal constant MAX_POSSIBLE_MOVES = uint8(2);
+    uint8 internal constant MAX_POSSIBLE_MOVES = uint8(3);
     IERC20 constant internal WETH_ADDRESS = IERC20(0xc778417E063141139Fce010982780140Aa0cD5Ab);
     
     struct CastedMove {
-        bytes32 committed;                         // Hash of the move casted by the player
-        uint8 revealed;                              // Revealed move submitted by the player
+        bytes32 committedMove;                         // Hash of the move casted by the player
+        uint8 revealedMove;                              // Revealed move submitted by the player
     }
     
     struct Round {
@@ -44,7 +44,7 @@ contract RockPaperScissors {
 
     event RoundCreated(uint256 indexed roundId, uint256 wagerAmount);
     event MoveCommitted(uint256 indexed roundId, address indexed player, bytes32 commitment);
-    event MoveRevealed(uint256 indexed roundId, address indexed player, uint8 outcome, address revealer);
+    event MoveRevealed(uint256 indexed roundId, address indexed player, uint8 revealedMove, address revealer);
 
     /**
     * @dev Ensure a certain round exists
@@ -97,10 +97,10 @@ contract RockPaperScissors {
     */
     function commit(uint256 _roundId, bytes32 _commitment) external roundExists(_roundId) {
         CastedMove storage castedMove = roundRecords[_roundId].moves[msg.sender];
-        require(castedMove.committed == bytes32(0), ERROR_MOVE_ALREADY_COMMITTED);
+        require(castedMove.committedMove == bytes32(0), ERROR_MOVE_ALREADY_COMMITTED);
         _ensurePlayerCanCommit(_roundId, msg.sender);
 
-        castedMove.committed = _commitment;
+        castedMove.committedMove = _commitment;
         emit MoveCommitted(_roundId, msg.sender, _commitment);
     }
 
@@ -118,7 +118,7 @@ contract RockPaperScissors {
     * @notice Reveal `_committedMove` round of `_player` for round #`_roundId`
     * @param _roundId ID of the round instance to reveal a move of
     * @param _player Address of the player to reveal a move for
-    * @param _committedMove Committed move revealed by the player
+    * @param _committedMove Committed move revealed by the player to be revealed
     * @param _salt Salt to decrypt and validate the committed move of the player
     */
     function reveal(uint256 _roundId, address _player, uint8 _committedMove, bytes32 _salt) external roundExists(_roundId) {
@@ -127,7 +127,7 @@ contract RockPaperScissors {
         _checkValidSalt(castedMove, _committedMove, _salt);
         require(_isValidCommittedMove(round, _committedMove), ERROR_INVALID_COMMITED_MOVE);
 
-        castedMove.revealed = _committedMove;
+        castedMove.revealedMove = _committedMove;
         emit MoveRevealed(_roundId, _player, _committedMove, msg.sender);
     }
 
@@ -140,7 +140,7 @@ contract RockPaperScissors {
     function getWinningMove(uint256 _roundId) external view roundExists(_roundId) returns (uint8) {
         Round storage round = roundRecords[_roundId];
         uint8 winningMove = round.winningMove;
-        return winningMove == REVEALED_MOVE_MISSING ? OUTCOME_REFUSED : winningOutcome;
+        return winningMove == REVEALED_MOVE_MISSING ? REVEALED_MOVE_REFUSED : winningMove;
     }
 
     /**
@@ -165,13 +165,13 @@ contract RockPaperScissors {
 
     /**
     * @dev Internal function to check if a move can be revealed for the given commited move and salt
-    * @param _castMove Casted move to be revealed
+    * @param _castedMove Casted move to be revealed
     * @param _commitedMove Move thas has to be proved
     * @param _salt Salt to decrypt and validate the provided move for a casted move
     */
-    function _checkValidSalt(CastedMove storage _castMove, uint8 _commitedMove, bytes32 _salt) internal view {
-        require(_castMove.revealed == REVEALED_MOVE_MISSING, ERROR_MOVE_ALREADY_REVEALED);
-        require(_castMove.committed == _hashMove(_commitedMove, _salt), ERROR_INVALID_HASHING_SALT);
+    function _checkValidSalt(CastedMove storage _castedMove, uint8 _commitedMove, bytes32 _salt) internal view {
+        require(_castedMove.revealedMove == REVEALED_MOVE_MISSING, ERROR_MOVE_ALREADY_REVEALED);
+        require(_castedMove.committedMove == _hashMove(_commitedMove, _salt), ERROR_INVALID_HASHING_SALT);
     }
 
 }
